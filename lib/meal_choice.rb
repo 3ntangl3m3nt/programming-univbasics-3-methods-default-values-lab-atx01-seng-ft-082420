7b0b98fa59cd551d122335822513879dc3691c88
Stefan Lewyckyj <s80lewyckyj@outlook.com>
2020-08-17 03:16:08 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,6 @@ require 'pry'# Your code here
 
 def meal_choice(protien = "meat", veg1, veg2)
   puts "What a nutritious meal!"
-  puts "A plate of #{protien} with #{side1} and #{side2}."
-  return meal_choice
+   "A plate of #{protien} with #{side1} and #{side2}."
 end
 # For output purposes, use "puts" instead of "print" or "p"
